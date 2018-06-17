7d3c18b14f04105e9f54673043a805b0a0957a4f
Oleg <User@DESKTOP-JNRD69N>
2018-06-17 12:18:45 +0300

    Init

---
+++ GarbageCollector/GarbageCollector.Part1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GarbageCollector.Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
